9f588fb0baf6b2145a20eae67d5b790c68975524
Thxsma <155584895+Thxsma@users.noreply.github.com>
2024-01-05 00:30:37 +0100

    Update Hotel.java

--- src/main/java/Classes/Hotel.java
+++ src/main/java/Classes/Hotel.java
@@ -1,22 +1,57 @@
-package Classes;
-
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class Hotel{
 
-  private String nom ;
-  private int nbEtoile, nbClient, nbChambre ;
-  private ArrayList<Chambre> chambres ;
+    protected String nom ;
+    protected int nbEtoile, nbClient, nbChambre ;
+    protected ArrayList<Chambre> chambres ;
 
-  public Hotel(String nom, int nbEtoile, int nbClient, int nbChambre, ArrayList<Chambre> chambres) {
-    this.nom = nom;
-    this.nbEtoile = nbEtoile;
-    this.nbClient = nbClient;
-    this.nbChambre = nbChambre;
-    this.chambres = chambres;
-  }
+    public Hotel(String nom,int nbEtoile,int nbClient,int nbChambre,ArrayList<Chambre> chambres) {
+        this.nom = nom;
+        this.nbEtoile = nbEtoile;
+        this.nbClient = nbClient;
+        this.nbChambre = nbChambre;
+        this.chambres = chambres;
 
+    }
 
+    public int getNbChambre() {
+        return nbChambre;
+    }
 
+    public String getNom() {
+        return nom;
+    }
+
+    public ArrayList<Chambre> getChambres() {
+        return chambres;
+    }
+
+    public int getNbClient() {
+        return nbClient;
+    }
+
+    public int getNbEtoile() {
+        return nbEtoile;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setChambres(ArrayList<Chambre> chambres) {
+        this.chambres = chambres;
+    }
+
+    public void setNbChambre(int nbChambre) {
+        this.nbChambre = nbChambre;
+    }
+
+    public void setNbClient(int nbClient) {
+        this.nbClient = nbClient;
+    }
+
+    public void setNbEtoile(int nbEtoile) {
+        this.nbEtoile = nbEtoile;
+    }
 }
