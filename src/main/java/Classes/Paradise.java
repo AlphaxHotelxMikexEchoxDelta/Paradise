d2d7434d87e575b039402d5952aac487594fba1e
Thxsma <155584895+Thxsma@users.noreply.github.com>
2024-01-05 00:28:06 +0100

    Update Paradise.java

--- src/main/java/Classes/Paradise.java
+++ src/main/java/Classes/Paradise.java
@@ -1,21 +1,28 @@
-package Classes;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Paradise extends Hotel /*implements MethodsHotel*/ {
 
-  private HashMap<Chambre,Client> reservations ;
+    private HashMap<Chambre,Client> reservations ;
 
-  public Paradise(String nom, int nbEtoile, int nbClient, int nbChambre, ArrayList<Chambre> chambres) {
-    super(nom, nbEtoile, nbClient, nbChambre, chambres);
-  }
+    public Paradise(String nom, int nbEtoile, int nbClient, int nbChambre, ArrayList<Chambre> chambres) {
+        super(nom, nbEtoile, nbClient, nbChambre, chambres);
+    }
 
-  public String makeReservation( Chambre chambre, Client client ){
-    this.reservations.put( chambre, client );
-    return "Reservation effectuer" ;
-  }
+    public String makeReservation( Chambre chambre, Client client ){
+        this.reservations.put( chambre, client );
+        return "Reservation effectuer" ;
+    }
+    public String adandonReservation(Chambre chambre,Client client){
+        //...........................
+        return "Réservation annuler";
+    }
 
+    public HashMap<Chambre, Client> getReservations() {
+        return reservations;
+    }
 
-  
+    public void setReservations(HashMap<Chambre, Client> reservations) {
+        this.reservations = reservations;
+    }
 }
