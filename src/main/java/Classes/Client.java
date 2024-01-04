46bfc4bb81fe9d6fcb659bfa520bc0bbe394f1b0
Thxsma <155584895+Thxsma@users.noreply.github.com>
2024-01-05 00:22:15 +0100

    Update Client.java

--- src/main/java/Classes/Client.java
+++ src/main/java/Classes/Client.java
@@ -1,16 +1,54 @@
-package Classes;
-
 public class Client {
     private String nom;
     private String prenom;
     private String numero;
-    private String idclient;
+    private int idclient;
     private String email ;
-    public Client(String nom,String prenom,String numero,String idclient){
+    public Client(String nom,String prenom,String numero,int idclient,String email){
         this.nom=nom;
         this.prenom=prenom;
         this.numero=numero;
         this.idclient=idclient;
         this.email=email;
+    }
+
+    public int getIdclient() {
+        return idclient;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setIdclient(int idclient) {
+        this.idclient = idclient;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
