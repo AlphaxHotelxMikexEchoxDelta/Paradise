7c2766d86902d23ea90e77d04f5281c6387daab0
Thxsma <155584895+Thxsma@users.noreply.github.com>
2024-01-05 13:48:55 +0100

    Update and rename ChambreDeLuxeLitDouble.java to LuxeLitDouble.java

--- src/main/java/Classes/LuxeLitDouble.java
+++ src/main/java/Classes/LuxeLitDouble.java
@@ -1,8 +1,8 @@
 package Classes;
 
-public class ChambreDeLuxeLitDouble extends Chambre {
+public class LuxeLitDouble extends Chambre {
     private int prix;
-    public ChambreDeLuxeLitDouble(int nbChambre,int id,boolean occupe,int prix){
+    public LuxeLitDouble(int nbChambre,int id,boolean occupe,int prix){
         super(nbChambre,id,occupe);
         this.prix=prix;
     }
