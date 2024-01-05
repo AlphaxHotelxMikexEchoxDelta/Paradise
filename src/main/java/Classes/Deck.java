99a5692ce5c3287c43394e0c7f7bae2a6c9fbe50
Thxsma <155584895+Thxsma@users.noreply.github.com>
2024-01-05 13:47:34 +0100

    Update Deck.java

--- src/main/java/Classes/Deck.java
+++ src/main/java/Classes/Deck.java
@@ -5,18 +5,17 @@ public class Deck {
     private LinkedList<Chambre> cardLinkedList = new LinkedList<Chambre>();
     public Deck(){
 
-        // Creation de chambre, et seulement de Chambre
         for(int i=0;i<=19;i++){
-            cardLinkedList.add(new ChambreClassiqueLitSimple(i,1,true,60));
+            cardLinkedList.add(new ClassiqueLitSimple(i,1,true,60));
         }
         for(int i=20;i<=40;i++){
-            cardLinkedList.add(new ChambreClassiqueLitDouble(i,2,true,70));
+            cardLinkedList.add(new ClassiqueLitDouble(i,2,true,70));
         }
         for (int i=100;i<=109;i++){
-            cardLinkedList.add(new ChambreDeLuxeLitSimple(i,3,true,100));
+            cardLinkedList.add(new LuxeLitSimple(i,3,true,100));
         }
         for(int i=110;i<=120;i++){
-            cardLinkedList.add(new ChambreDeLuxeLitDouble(i,4,true,110));
+            cardLinkedList.add(new  LuxeLitDouble(i,4,true,110));
         }
     }
 
